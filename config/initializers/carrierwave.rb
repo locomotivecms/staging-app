b7c02e29d7cb167977712a385fdb7307862f179b
Didier Lafforgue <didier.lafforgue@gmail.com>
2024-08-31 11:39:07 +0200

    don't initialize AWS client when precompiling the assets (Docker)

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,30 +9,35 @@ CarrierWave.configure do |config|
     config.root = File.join(Rails.root, 'public')
 
   when :production
-    # config.storage = :file
-    # config.root = File.join(Rails.root, 'public')
+    begin
+      # context: when we're precompiling the assets in the Dockerfile
+      break if ENV['S3_BUCKET'].blank?
 
-    # # put your CDN host below instead
-    # # config.asset_host = 'http://cdn.example.com'
+      # config.storage = :file
+      # config.root = File.join(Rails.root, 'public')
 
-    # # You can also use Amazon S3 instead. The following configuration works for AWS
-    # #
-    # # WARNING: add the "carrierwave-aws" gem in your Rails app Gemfile.
-    # # More information here: https://github.com/sorentwo/carrierwave-aws
-    #
-    config.storage          = :aws
-    config.aws_bucket       = ENV['S3_BUCKET']
-    config.aws_acl          = 'public-read'
+      # # put your CDN host below instead
+      # # config.asset_host = 'http://cdn.example.com'
 
-    config.aws_credentials  = {
-      access_key_id:      ENV['S3_KEY_ID'],
-      secret_access_key:  ENV['S3_SECRET_KEY'],
-      region:             ENV['S3_BUCKET_REGION']
-    }
+      # # You can also use Amazon S3 instead. The following configuration works for AWS
+      # #
+      # # WARNING: add the "carrierwave-aws" gem in your Rails app Gemfile.
+      # # More information here: https://github.com/sorentwo/carrierwave-aws
+      #
+      config.storage          = :aws
+      config.aws_bucket       = ENV['S3_BUCKET']
+      config.aws_acl          = 'public-read'
 
-    # Put your CDN host below instead
-    if ENV['S3_ASSET_HOST_URL'].present?
-      config.asset_host = ENV['S3_ASSET_HOST_URL']
+      config.aws_credentials  = {
+        access_key_id:      ENV['S3_KEY_ID'],
+        secret_access_key:  ENV['S3_SECRET_KEY'],
+        region:             ENV['S3_BUCKET_REGION']
+      }
+
+      # Put your CDN host below instead
+      if ENV['S3_ASSET_HOST_URL'].present?
+        config.asset_host = ENV['S3_ASSET_HOST_URL']
+      end
     end
 
   else
