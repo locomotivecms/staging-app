cb8f49788862e2558e75ead479a29346bb10fa37
Didier Lafforgue <didier.lafforgue@gmail.com>
2024-08-31 23:08:00 +0200

    run Sidekiq + declare a Redis server

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,13 @@
 require 'hiredis'
 require 'active_support/cache/redis_cache_store'
 
+begin
+  redis = Redis.new
+  pp redis.ping
+rescue Exception => e
+  pp "🚨🚨🚨 #{e.message}"
+end
+
 # UPDATE (03/02/2023): not required apparently
 # My attempt to fix this inconsistent behavior of HiRedis. The issue is described here:
 # https://github.com/redis/hiredis-rb/issues/40
